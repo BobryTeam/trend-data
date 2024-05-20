3630b3a4e2f35a1288c68ce7bf126e4e7dd46f3c
vitosotdihaet <vitostikitos@gmail.com>
2024-05-20 17:23:39 +0300

    Add a TrendData skeleton

---
+++ trend-data.py
@@ -0,0 +1,19 @@
+class TrendData:
+    def __init__(self):
+        '''
+        Инициализация класса
+        '''
+        pass
+
+    def __str__(self) -> str:
+        '''
+        Превращает скейл дату в строку
+        '''
+        return ''
+
+class TrendDataFromStr(TrendData):
+    def __init__(self, string_data: str):
+        '''
+        Инициализация класса из строки
+        '''
+        return super().__init__()
